b73602273b9daaa6ddd55e15bd47478fccaf5429
Kevin Tandavo <riyuze211001@gmail.com>
2023-08-15 20:09:01 +0700

    Changed Settings

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
         port: 3000,
         https: true,
     },
-    base: "/",
+    base: "https://riyuze.github.io/shopediasobu-web/",
     plugins: [basicSsl(), react()],
 });
