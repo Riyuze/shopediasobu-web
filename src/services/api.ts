6c6f9caa726d2ccd1f4e349cdcb06d3b3e85839b
Kevin Tandavo <riyuze211001@gmail.com>
2023-08-15 19:29:46 +0700

    Changed API endpoint to Deployed Endpoint

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,25 +1,12 @@
-import axios, { AxiosError, InternalAxiosRequestConfig } from "axios";
+import axios from "axios";
 
 const api = axios.create({
     // baseURL: import.meta.env.VITE_API_URL,
-    baseURL: "http://localhost:8080/api",
+    baseURL: "https://shopediasobu-api.vercel.app/api/",
 });
 
 export const apiFetcher = (resource: string) =>
     api.get(resource).then((res) => res.data);
-
-// api.interceptors.request.use(
-//     (config: InternalAxiosRequestConfig): InternalAxiosRequestConfig => {
-//         const token = localStorage.getItem("token");
-//         if (token) {
-//             config.headers.Authorization = `Bearer ${token}`;
-//         }
-//         return config;
-//     },
-//     (error: AxiosError): Promise<AxiosError> => {
-//         return Promise.reject(error);
-//     }
-// );
 
 api.interceptors.response.use(
     (response) => {
